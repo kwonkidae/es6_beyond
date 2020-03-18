6d10e287e35463f57930b32dcf06237514070e8e
kwon <kkdosk@naver.com>
2020-03-18 10:53:16 +0900

    iter

--- ch03/iter.js
+++ ch03/iter.js
@@ -1,4 +1,5 @@
 (() => {
+  return;
   const Fib = {
     [Symbol.iterator]() {
       var n1 = 1,
@@ -27,5 +28,98 @@
   for (var v of Fib[Symbol.iterator]()) {
     console.log(v);
     if (v > 10) break;
+  }
+})();
+
+(() => {
+  let tasks = {
+    [Symbol.iterator]() {
+      var steps = this.actions.slice();
+
+      return {
+        [Symbol.iterator]() {
+          return this;
+        },
+
+        next(...args) {
+          if (steps.length > 0) {
+            let res = steps.shift()(...args);
+            return { value: res, done: false };
+          } else {
+            return { done: true };
+          }
+        },
+
+        return(v) {
+          steps.length = 0;
+          return { value: v, done: true };
+        },
+      };
+    },
+    actions: [],
+  };
+
+  tasks.actions.push(
+    function step1(x) {
+      console.log('step 1: ', x);
+      return x * 2;
+    },
+    function step2(x, y) {
+      console.log('step 2: ', x, y);
+      return x + y * 2;
+    },
+    function step3(x, y, z) {
+      console.log('step 3: ', x, y, z);
+      return x * y + z;
+    },
+  );
+
+  var it = tasks[Symbol.iterator]();
+
+  it.next(10);
+  it.next(20, 50);
+  it.next(20, 50, 120);
+
+  it.next();
+})();
+
+(() => {
+  if (!Number.prototype[Symbol.iterator]) {
+    Object.defineProperty(Number.prototype, Symbol.iterator, {
+      writable: true,
+      configurable: true,
+      enumerable: false,
+      value: function iterator() {
+        var i,
+          inc,
+          done = false,
+          top = +this;
+        inc = 1 * (top < 0 ? -1 : 1);
+        return {
+          [Symbol.iterator]() {
+            return this;
+          },
+          next() {
+            if (!done) {
+              if (i == null) {
+                i = 0;
+              } else if (top >= 0) {
+                i = Math.min(top, i + inc);
+              } else {
+                i = Math.max(top, i + inc);
+              }
+              if (i == top) done = true;
+              return { value: i, done: false };
+            } else {
+              return { done: true };
+            }
+          },
+        };
+      },
+    });
+  }
+
+  for (var i of -3) {
+    console.log(i);
   }
 })();
